e6d6727a678493955d99f06d94a517f806cff8e1
YeLinAung <40236753+b14cknc0d3@users.noreply.github.com>
2026-04-07 10:48:34 +0630

    feat: solved valid palindrome

--- src/easy/valid_palindrome.rs
+++ src/easy/valid_palindrome.rs
@@ -12,7 +12,28 @@ struct Solution;
 
 impl Solution {
     pub fn is_palindrome(s: String) -> bool {
-        todo!()
+        let (mut left, mut right) = (0, s.len() - 1);
+        let chars: Vec<char> = s.chars().collect();
+        if chars.is_empty() {
+            return true;
+        }
+        while left < right {
+            if !chars[left].is_alphanumeric() {
+                left += 1;
+                continue;
+            }
+            if !chars[right].is_alphanumeric() {
+                right -= 1;
+                continue;
+            }
+            if chars[left].to_ascii_lowercase() != chars[right].to_ascii_lowercase() {
+                return false;
+            }
+
+            left += 1;
+            right -= 1;
+        }
+        true
     }
 }
 
