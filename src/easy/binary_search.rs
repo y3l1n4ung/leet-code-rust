2a383da60390a9ddaedc89b5c58cc1351f54a666
YeLinAung <40236753+b14cknc0d3@users.noreply.github.com>
2026-04-11 09:14:41 +0630

    feat: solved Binary Search.

--- src/easy/binary_search.rs
+++ src/easy/binary_search.rs
@@ -1,3 +1,5 @@
+use core::num;
+
 /// [704] Binary Search
 /// Difficulty: Easy
 /// Topics: Array, Binary Search
@@ -13,7 +15,22 @@ struct Solution;
 
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        todo!()
+        let mut low: i32 = 0;
+        let mut high: i32 = (nums.len() - 1) as i32;
+
+        while low <= high {
+            let mid: i32 = low + (high - low) / 2;
+            let val: i32 = nums[mid as usize];
+            if target == val {
+                return mid as i32;
+            } else if target > val as i32 {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        -1
     }
 }
 
@@ -34,5 +51,14 @@ mod tests {
     #[test]
     fn test_3() {
         assert_eq!(Solution::search(vec![5], 5), 0);
+    }
+    #[test]
+    fn test_4() {
+        assert_eq!(Solution::search(vec![5], -5), -1);
+    }
+
+    #[test]
+    fn test_5() {
+        assert_eq!(Solution::search(vec![-1, 0, 3, 5, 9, 12], 0), 1);
     }
 }
