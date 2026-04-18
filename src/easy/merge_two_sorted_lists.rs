613133350231b8412bd60fcfc41a9c97592a89d4
YeLinAung <40236753+b14cknc0d3@users.noreply.github.com>
2026-04-18 14:43:03 +0630

    feat: solved merged two sorted lists.

--- src/easy/merge_two_sorted_lists.rs
+++ src/easy/merge_two_sorted_lists.rs
@@ -12,25 +12,44 @@
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
 }
 
 impl ListNode {
-  #[inline]
-  fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
     }
-  }
 }
 
 struct Solution;
 
 impl Solution {
-    pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        todo!()
+    pub fn merge_two_lists(
+        mut list1: Option<Box<ListNode>>,
+        mut list2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let mut dummy = ListNode::new(0);
+        let mut current = &mut dummy.next;
+
+        while list1.is_some() && list2.is_some() {
+            let lh = list1.as_ref()?.val;
+            let rh = list2.as_ref()?.val;
+            if lh <= rh {
+                let mut node = list1.take()?;
+                list1 = node.next.take();
+
+                *current = Some(node);
+            } else {
+                let mut node = list2.take()?;
+                list2 = node.next.take();
+                *current = Some(node);
+            }
+            current = &mut current.as_mut().unwrap().next;
+        }
+        *current = list1.or(list2);
+        dummy.next
     }
 }
 
